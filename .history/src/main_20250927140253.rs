f8de6fd915b0cdecdd31d2bfd0069ecbd0e04aa6
Guilhem-LsM <guilhem.lesaint.marquet@gmail.com>
2025-09-27 15:34:04 +0200

    Delete iced, add minifb

---
+++ .history/src/main_20250927140253.rs
@@ -0,0 +1,17 @@
+fn main() -> iced::Result {
+    iced::run("EGP-Designer", MyApp::update, MyApp::view)
+}
+
+type Message = ();
+
+#[derive(Default)]
+struct MyApp;
+
+impl MyApp {
+    fn update(&mut self, _message: Message) {}
+
+    fn view(&self) -> iced::Element<Message> {
+        "Hello World!".into()
+    }
+}
+
